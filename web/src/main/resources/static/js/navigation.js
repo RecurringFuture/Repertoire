0ac446b317c8b770b24e9f7948e5448fcc8a532f
HH <hahn@recurringfuture.com>
2026-01-12 07:25:37 +0100

    songs in listbox II

--- web/src/main/resources/static/js/navigation.js
+++ web/src/main/resources/static/js/navigation.js
@@ -4,8 +4,7 @@ function songsFunction() {
     document.getElementById("songsDropdown").classList.toggle("show");
 }
 
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(e) {
+document.getElementById("songsButton").addEventListener("mouseout", (event) => {
     if (!e.target.matches('.dropbtn')) {
         var songsDropdown = document.getElementById("songsDropdown");
         if (songsDropdown.classList.contains('show')) {
@@ -16,7 +15,21 @@ window.onclick = function(e) {
             myDropdown.classList.remove('show');
         }
     }
-}
+})
+
+// Close the dropdown if the user clicks outside of it
+// window.onclick = function(e) {
+//     if (!e.target.matches('.dropbtn')) {
+//         var songsDropdown = document.getElementById("songsDropdown");
+//         if (songsDropdown.classList.contains('show')) {
+//             songsDropdown.classList.remove('show');
+//         }
+//         var myDropdown = document.getElementById("myDropdown");
+//         if (myDropdown.classList.contains('show')) {
+//             myDropdown.classList.remove('show');
+//         }
+//     }
+// }
 
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
