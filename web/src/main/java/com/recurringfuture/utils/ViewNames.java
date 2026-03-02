7b22c0cab5ca50f6187a14083888fbcc9751c5ee
HH <hahn@recurringfuture.com>
2026-03-02 15:04:15 +0100

    webconfig etc.

--- web/src/main/java/com/recurringfuture/utils/ViewNames.java
+++ web/src/main/java/com/recurringfuture/utils/ViewNames.java
@@ -7,6 +7,8 @@ public final class ViewNames {
     public static final String IMPORT_SONGS = "importSongs";
     public static final String ADD_SONG = "addSong";
     public static final String PROJECTS = "projects";
+    public static final String PRACTICE = "practice";
+    public static final String SETTINGS = "settings";
 
     private ViewNames() {}
 }
