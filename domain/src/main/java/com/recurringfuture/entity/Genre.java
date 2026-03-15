2f7d3d8eeaa74dab886b53881aca6c159f896c4b
Recurring Future <hahn@recurringfuture.com>
2026-03-15 15:21:23 +0100

    genre tabel

--- domain/src/main/java/com/recurringfuture/entity/Genre.java
+++ domain/src/main/java/com/recurringfuture/entity/Genre.java
@@ -5,13 +5,18 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
+import lombok.NonNull;
 
-@Entity(name = "tuning")
+@Entity(name="genre")
 @Data
-public class Tuning {
+public class Genre {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    private String tuning;
+    @NonNull
+    private String title;
 
+    public Genre() {
+
+    }
 }
