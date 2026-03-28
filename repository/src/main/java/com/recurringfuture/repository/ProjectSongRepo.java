d14034ee041352446d4b4bb0c4342566bdae6046
Recurring Future <hahn@recurringfuture.com>
2026-03-28 21:31:51 +0100

    projectrepo custom query

--- repository/src/main/java/com/recurringfuture/repository/ProjectSongRepo.java
+++ repository/src/main/java/com/recurringfuture/repository/ProjectSongRepo.java
@@ -2,11 +2,13 @@ package com.recurringfuture.repository;
 
 import com.recurringfuture.entity.ProjectSong;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface ProjectSongRepo extends JpaRepository<ProjectSong, Integer> {
+    @Query(value = "SELECT * FROM project_song WHERE project_id = ?1", nativeQuery = true)
     List<ProjectSong> findByProjectId(int projectId);
 }
