38acd4089877627ddbf22e4c965d1f2adf0b2154
HH <hahn@recurringfuture.com>
2026-04-13 13:48:52 +0200

    tables fixed

--- repository/src/main/java/com/recurringfuture/repository/data/RepertoireData.java
+++ repository/src/main/java/com/recurringfuture/repository/data/RepertoireData.java
@@ -13,10 +13,10 @@ public class RepertoireData {
     }
 
     public static List<String> getCapoPositions() {
-        return List.of("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12");
+        return List.of("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12");
     }
 
     public static List<String> getThresholds() {
-        return List.of("1 Day", "2 Days", "3 Days", "4 Days", "5 Days", "1 Week", "2 Weeks", "3 Weeks", "1 Month", "2 Months", "3 Months");
+        return List.of("None", "1 Day", "2 Days", "3 Days", "4 Days", "5 Days", "1 Week", "2 Weeks", "3 Weeks", "1 Month", "2 Months", "3 Months");
     }
 }
