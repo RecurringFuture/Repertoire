69917e7121c6f83a5308f0e055e06654b1a32955
hh <hahn@recurringfuture.com>
2026-04-26 22:41:59 +0200

    Requestmapping practice added

--- web/src/main/java/com/recurringfuture/controller/PracticeController.java
+++ web/src/main/java/com/recurringfuture/controller/PracticeController.java
@@ -9,9 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
 @Controller
+@RequestMapping("/practice")
 public class PracticeController {
 
     private static final Logger logger = LoggerFactory.getLogger(PracticeSetController.class);
