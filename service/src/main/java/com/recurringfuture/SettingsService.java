fed3999219db5c9ba222a9fa0a3a19e5cb800a9e
hh <hahn@recurringfuture.com>
2026-05-11 17:41:34 +0200

    Settings page

--- service/src/main/java/com/recurringfuture/SettingsService.java
+++ service/src/main/java/com/recurringfuture/SettingsService.java
@@ -1,5 +1,7 @@
 package com.recurringfuture;
 
+import com.recurringfuture.entity.Genre;
+import com.recurringfuture.entity.Tuning;
 import com.recurringfuture.repository.GenreRepo;
 import com.recurringfuture.repository.TuningRepo;
 import lombok.extern.slf4j.Slf4j;
@@ -7,6 +9,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Slf4j
 @Service
@@ -21,5 +25,21 @@ public class SettingsService {
     public SettingsService(TuningRepo tuningRepo, GenreRepo genreRepo) {
         this.tuningRepo = tuningRepo;
         this.genreRepo = genreRepo;
+    }
+
+    public void addTuning(Tuning tuning) {
+        tuningRepo.save(tuning);
+    }
+
+    public void addGenre(Genre genre) {
+        genreRepo.save(genre);
+    }
+
+    public List<Tuning> getTunings() {
+        return tuningRepo.findAll();
+    }
+
+    public List<Genre> getGenres() {
+        return genreRepo.findAll();
     }
 }
--- web/src/main/java/com/recurringfuture/controller/SettingsController.java
+++ web/src/main/java/com/recurringfuture/controller/SettingsController.java
@@ -7,10 +7,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
 @Controller
+@RequestMapping("/settings")
 public class SettingsController {
 
     private static final Logger logger = LoggerFactory.getLogger(SettingsController.class);
@@ -22,8 +25,15 @@ public class SettingsController {
         this.settingsService = settingsService;
     }
 
-    @GetMapping("/settings")
+    @GetMapping("/")
     public String showSettingsPage() {
         return ViewNames.SETTINGS;
+    }
+
+    @GetMapping("/manageTunings")
+    public String manageTunings(Model model) {
+        logger.info("GET TUNINGS TO MANAGE");
+        model.addAttribute("tunings", settingsService.getTunings());
+        return "redirect:/settings";
     }
 }
