12309508f10c76ce2889b83c900fc26157c42165
hh <hahn@recurringfuture.com>
2026-05-13 09:26:34 +0200

    manage delete

--- web/src/main/java/com/recurringfuture/controller/SettingsController.java
+++ web/src/main/java/com/recurringfuture/controller/SettingsController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
@@ -45,6 +46,13 @@ public class SettingsController {
         model.addAttribute("editMode", "Genres");
         model.addAttribute("itemsToManage", settingsService.getGenres());
         logger.info("GENRES TO MANAGE: " + model.getAttribute("itemsToManage"));
+        return ViewNames.SETTINGS;
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteItem(@PathVariable("id") Long id, Model model) {
+
+        logger.info("DELETE ITEM: " + model.getAttribute("editMode") + " ID: " + id);
         return ViewNames.SETTINGS;
     }
 
