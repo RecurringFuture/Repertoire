7bbdccdb08599a5311d8e9706b422cb802083b77
HH <hahn@recurringfuture.com>
2026-09-14 15:57:39 +0200

    minor changes

--- repository/src/main/java/com/recurringfuture/repository/data/RepertoireData.java
+++ repository/src/main/java/com/recurringfuture/repository/data/RepertoireData.java
@@ -5,15 +5,15 @@ import java.util.List;
 public class RepertoireData {
 
     public static List<String> getKeys() {
-        return List.of("C", "C#", "Db", "D", "D#", "Eb", "E", "F", "F#", "Gb", "G" , "G#", "Ab", "A", "A#", "Bb", "B");
+        return List.of("", "C", "C#", "Db", "D", "D#", "Eb", "E", "F", "F#", "Gb", "G" , "G#", "Ab", "A", "A#", "Bb", "B");
     }
 
     public static List<String> getStates() {
-        return List.of("ToDo", "In Progress", "Repertoire");
+        return List.of("", "ToDo", "In Progress", "Repertoire");
     }
 
     public static List<String> getCapoPositions() {
-        return List.of("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12");
+        return List.of("", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12");
     }
 
     public static List<String> getThresholds() {
